66bfff423a5fb5bb42b11a034699e142d1cc3874
Jack Farman <jack.farman.17@ucl.ac.uk>
2020-11-25 12:54:51 +0100

    created appointments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :gardens
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :gardens do
+    resources :appointments, only: [ :new, :create ]
+  end
 end
