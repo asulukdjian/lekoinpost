b94e7086232233b56ea7ae950b045512b45e9bc2
Aram Sulukdjian <asulukdjian@pm.me>
2020-11-25 16:30:53 +0100

    working on cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,18 +14,18 @@ User.destroy_all
 puts "users destroyed"
 
 User.create!(
-  first_name: "Clara", 
-  last_name: Faker::Name.last_name, 
-  email: "clara@mail.fr", 
+  first_name: "Clara",
+  last_name: Faker::Name.last_name,
+  email: "clara@mail.fr",
   password: "123456",
   password_confirmation: "123456",
   address: "44 rue Ordener, Paris"
 )
 
 User.create!(
-  first_name: "Gaston", 
-  last_name: Faker::Name.last_name, 
-  email: "gaston@mail.fr", 
+  first_name: "Gaston",
+  last_name: Faker::Name.last_name,
+  email: "gaston@mail.fr",
   password: "123456",
   password_confirmation: "123456",
   address: "97, rue Lénine, Bagnolet"
@@ -33,15 +33,15 @@ User.create!(
 
 Garden.create!(
     name: "Gaston's cité fertile",
-    description: "Jardin alternatif bio et collaboratif de Gaston", 
-    address: "14 avenue Edouard Vaillant, Pantin", 
+    description: "Jardin alternatif bio et collaboratif de Gaston",
+    address: "14 avenue Edouard Vaillant, Pantin",
     user_id: User.last.id
   )
 
 User.create!(
-  first_name: "Emilie", 
-  last_name: Faker::Name.last_name, 
-  email: "emilie@mail.fr", 
+  first_name: "Emilie",
+  last_name: Faker::Name.last_name,
+  email: "emilie@mail.fr",
   password: "123456",
   password_confirmation: "123456",
   address: "71 Rue Henri Langlois, Bobigny"
@@ -49,16 +49,16 @@ User.create!(
 
 Garden.create!(
     name: "Emilie's shared garden",
-    description: "Jardin alternatif bio et collaboratif de Emilie", 
-    address: "4 Rue Henri Barbusse, Aubervilliers", 
+    description: "Jardin alternatif bio et collaboratif de Emilie",
+    address: "4 Rue Henri Barbusse, Aubervilliers",
     user_id: User.last.id
   )
 
 
 User.create!(
-  first_name: "Paul", 
-  last_name: Faker::Name.last_name, 
-  email: "paul@mail.fr", 
+  first_name: "Paul",
+  last_name: Faker::Name.last_name,
+  email: "paul@mail.fr",
   password: "123456",
   password_confirmation: "123456",
   address: "71 Rue Henri Langlois, Bobigny"
@@ -66,8 +66,8 @@ User.create!(
 
 Garden.create!(
     name: "Paul in Guinot garden",
-    description: "Jardin alternatif bio et collaboratif de Paul", 
-    address: " 4 Rue Claude Guinot, Saint-Ouen", 
+    description: "Jardin alternatif bio et collaboratif de Paul",
+    address: " 4 Rue Claude Guinot, Saint-Ouen",
     user_id: User.last.id
   )
 
