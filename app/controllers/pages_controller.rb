a975af1ad28254c1726eb29ba20e81229db74d2a
Gueram Minassian <gueram.minassian@gmail.com>
2020-11-30 18:18:07 +0100

    full-calendar-basic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,15 @@ class PagesController < ApplicationController
     @my_appointments = current_user.appointments
     @my_gardens = current_user.gardens
     @my_reserved_gardens = current_user.reserved_gardens
+
+    @events = current_user.reservations.map do |appointment|
+    {
+      title: appointment.user.first_name,
+      start: appointment.date.strftime('%Y-%m-%d'),
+      end: (appointment.date + 1.hour).strftime('%Y-%m-%d')
+    }
+end
+    
   end
 
   def home
